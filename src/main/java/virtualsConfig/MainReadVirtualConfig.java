ab2e4ab36c731c3d5f6d068fae0ce81ac6fbee39
nik <nikit.cpp@yandex.ru>
2015-02-22 16:05:44 +0300

    initial working validation

---
+++ src/main/java/virtualsConfig/MainReadVirtualConfig.java
@@ -0,0 +1,51 @@
+package virtualsConfig;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.ValidationException;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+
+import com.thoughtworks.xstream.XStream;
+
+public class MainReadVirtualConfig {
+
+	public static void main(String[] args) throws FileNotFoundException {		
+		XStream xstream = new XStream();
+		
+		Class[] annotated = new Class[] {VmRunWorkstation.class , User.class, WorkstationEthernetDevice.class, VirtualsConfig.class};
+		xstream.processAnnotations(annotated);
+		InputStream is = new FileInputStream("target/virtuals.xml");
+		VirtualsConfig c = (VirtualsConfig) xstream.fromXML(is);
+        
+        Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
+
+		validate(c, validator);		
+	}
+	
+	
+    public static void validate(Object object, Validator validator) throws ValidationException {
+        Set<ConstraintViolation<Object>> constraintViolations = validator
+                .validate(object);
+        if(constraintViolations.size()!=0){
+            System.err.println("[Hibernate Validator] Validation erors:");
+            for (ConstraintViolation<Object> cv : constraintViolations)
+                System.err.println(String.format(
+                        "property: [%s], value: [%s], message: [%s]",
+                        cv.getPropertyPath(), cv.getInvalidValue(), cv.getMessage()));
+
+            throw new ValidationException();
+        }
+    }
+
+
+}
--- src/main/java/virtualsConfig/MainWriteVirtualConfig.java
+++ src/main/java/virtualsConfig/MainWriteVirtualConfig.java
@@ -5,10 +5,15 @@ import java.io.FileOutputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.ValidationException;
+import javax.validation.Validator;
 
 import com.thoughtworks.xstream.XStream;
 
-public class MainVirtualConfig {
+public class MainWriteVirtualConfig {
 
 	public static void main(String[] args) throws FileNotFoundException {
 		User u = new User("mylogin", "myPassword");
